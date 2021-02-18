65e25e682f0eb57a645c6953b50f7b7b729b108f
gabrielnndoan <gabrielnndoan@gmail.com>
2021-02-17 20:50:36 -0600

    finished part 2 of workshop part 2

--- api/index.js
+++ api/index.js
@@ -1,13 +1,87 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
 
-const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+const jwt = require("jsonwebtoken");
+const { getUserById } = require("../db");
+const { JWT_SECRET } = process.env;
 
-const postsRouter = require('./posts');
-apiRouter.use('/posts', postsRouter)
+// apiRouter.use(async (req, res, next) => {
+//   const prefix = "Bearer ";
+//   const auth = req.headers["Authorization"];
 
-const tagsRouter = require('./tags');
-apiRouter.use('/tags', tagsRouter)
+//   if (!auth) {
+//     next(); // don't set req.user, no token was passed in
+//   }
+
+//   if (auth.startsWith(prefix)) {
+//     // recover the token
+//     const token = auth.slice(prefix.length);
+//     try {
+//       // recover the data
+//       const { id } = jwt.verify(token, JWT_SECRET);
+
+//       // get the user from the database
+//       const user = await getUserById(id);
+//       // note: this might be a user or it might be null depending on if it exists
+
+//       // attach the user and move on
+//       req.user = user;
+
+//       next();
+//     } catch (error) {
+//       // there are a few types of errors here
+//       console.log(error)
+//     }
+//   }
+// });
+
+// set `req.user` if possible
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+        console.log(id)
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const postsRouter = require("./posts");
+apiRouter.use("/posts", postsRouter);
+
+const tagsRouter = require("./tags");
+apiRouter.use("/tags", tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send(error);
+});
 
 module.exports = apiRouter;
